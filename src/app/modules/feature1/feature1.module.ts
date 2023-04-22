9e82a85f0087205d2c9f9f75f5083e22e6fa5356
Fran Merlini <merlinifrancisco@gmail.com>
2023-04-22 01:25:03 -0300

    feat: add missing modules

---
+++ src/app/modules/feature1/feature1.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { Feature1RoutingModule } from './feature1-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, Feature1RoutingModule],
+})
+export class Feature1Module {}
